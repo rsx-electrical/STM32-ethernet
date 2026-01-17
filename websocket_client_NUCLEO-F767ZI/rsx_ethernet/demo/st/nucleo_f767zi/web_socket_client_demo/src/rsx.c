ec739d01177f061a9e035127c42071278ee98718
Tanmay337442 <tanmay337442@gmail.com>
2026-01-17 13:49:30 -0500

    rsx task

--- websocket_client_NUCLEO-F767ZI/rsx_ethernet/demo/st/nucleo_f767zi/web_socket_client_demo/src/rsx.c
+++ websocket_client_NUCLEO-F767ZI/rsx_ethernet/demo/st/nucleo_f767zi/web_socket_client_demo/src/rsx.c
@@ -1,15 +1,6 @@
 #include <stdlib.h>
+
 #include "stm32f7xx.h"
 #include "stm32f7xx_hal.h"
 
-int main(void) {
-    HAL_Init();            // Resets all peripherals and initializes Flash interface
-    SystemClock_Config();  // Sets the speed of the processor
-    MX_GPIO_Init();        // Powers up the pins you configured
-    MX_ADC1_Init();        // Powers up the voltage/current sensors
-    MX_SPI1_Init();        // Powers up the BMS communication
-
-    while (1) {
-        
-    }
-}
+void Estop_toggle(void) {}
