028e718e2ae7e721d4a367dae1960fb6bf78d7c0
Tanmay337442 <tanmay337442@gmail.com>
2026-01-17 15:54:55 -0500

    modified rsx.h

--- websocket_client_NUCLEO-F767ZI/rsx_ethernet/demo/st/nucleo_f767zi/web_socket_client_demo/src/rsx.h
+++ websocket_client_NUCLEO-F767ZI/rsx_ethernet/demo/st/nucleo_f767zi/web_socket_client_demo/src/rsx.h
@@ -6,11 +6,19 @@
 #include "stm32f7xx.h"
 #include "stm32f7xx_hal.h"
 
+typedef struct {
+  float v_5v, v_12v, v_19v, v_24v, v_55v;
+  float v_batt_adc, v_batt_bms;
+  float i_motor, i_charger, i_batt;
+} Measurements;
+
 // Define the pins based on the diagram provided
 #define MOTOR_EN_PORT GPIOD
 #define MOTOR_EN_PIN GPIO_PIN_11
+
 #define ARM_EN_PORT GPIOE
 #define ARM_EN_PIN GPIO_PIN_6
+
 #define BUS_5V_PORT GPIOF
 #define BUS_5V_PIN GPIO_PIN_15
 #define BUS_12V_PORT GPIOD
@@ -21,8 +29,30 @@
 #define BUS_24V_PIN GPIO_PIN_6
 #define BUS_55V_PORT GPIOB
 #define BUS_55V_PIN GPIO_PIN_2
+
 #define ESTOP_PORT GPIOG
 #define ESTOP_PIN GPIO_PIN_1
+
+#define MEASURE_5V_PORT GPIOB
+#define MEASURE_5V_PIN GPIO_PIN_1
+#define MEASURE_12V_PORT GPIOC
+#define MEASURE_12V_PIN GPIO_PIN_2
+#define MEASURE_19V_PORT GPIOA
+#define MEASURE_19V_PIN GPIO_PIN_3
+#define MEASURE_24V_PORT GPIOF
+#define MEASURE_24V_PIN GPIO_PIN_4
+#define MEASURE_55V_PORT GPIOF
+#define MEASURE_55V_PIN GPIO_PIN_10
+
+#define MEASURE_BATT_V_PORT GPIOC
+#define MEASURE_BATT_V_PIN GPIO_PIN_0
+
+#define MEASURE_MOTOR_A_PORT GPIOC
+#define MEASURE_MOTOR_A_PIN GPIO_PIN_3
+#define MEASURE_CHARGER_A_PORT GPIOF
+#define MEASURE_CHARGER_A_PIN GPIO_PIN_3
+#define MEASURE_BATT_A_PORT GPIOF
+#define MEASURE_BATT_A_PIN GPIO_PIN_5
 
 // Function Prototypes for MCU Functions
 void motor_on(void);
