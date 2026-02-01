0f509bb71768459c0b88654c001dc1d5ad9b46f6
cocolu76 <116968267+cocolu76@users.noreply.github.com>
2026-01-31 21:33:35 -0500

    adding new bms pec files

---
+++ websocket_client_NUCLEO-F767ZI/rsx_ethernet/demo/st/nucleo_f767zi/web_socket_client_demo/src/BMSlib.h
@@ -0,0 +1,37 @@
+#ifndef BMS_HEADER
+#define BMS_HEADER
+#include <stdint.h>
+extern const uint8_t ADCVSC[4]; //cmd and pec of cell voltage and sc conversion pole
+extern const uint8_t RDSTATB[12];
+extern const uint8_t RDCFG[12];
+extern const uint8_t WRCFG[12];
+extern const uint8_t WRCFG_data_starter[6]; //uint8_ts 1-3, 4 are set with discharge and uv/ov
+extern const uint8_t RDCVA[12];
+extern const uint8_t RDCVB[12];
+extern const uint8_t ADCV[4];
+extern const uint8_t CLRCELL[4];
+
+#define NUMCELLS 4
+
+void measure_batt_v(uint16_t* mv, int print);
+void wakeup(); //wake up spi interface (not working)
+void adcvsc(); //measure all cells and sum of all cells
+void adcv();  //measure all cells
+void rdstatb(); //read status register b
+void rdcfg(uint8_t *CFGR, int CFGR_size); //read configuration register into size 6 uint8_t array "CFGR"
+void wrcfg(uint8_t* data, int data_size); //write size 6 uint8_t array "data" into configuration register
+void rdvab(uint16_t* CV, int CVsize); //read voltage registers a & b into uint16_t size 6 array "CV"
+void clearcellreg(); //clear voltage registers
+uint8_t checkPEC(uint8_t pec0_received, uint8_t pec1_received, uint8_t* data, int data_len); //check pec of received uint8_ts
+void dataCollection(uint8_t c0, uint8_t c1, uint8_t c2, uint8_t c3, uint8_t c4);
+void dischargeCellX(uint8_t* data, int data_size, int cell_x); //cell_x takes values 1 to 6
+void SPItransfer(const uint8_t* buffer, uint16_t size);
+void print_msg(char * msg);
+void print_buffer(uint8_t * buffer, int buffer_size);
+void SPItransferReceive(const uint8_t* buffer, uint8_t* rx, uint16_t size);
+void SPItransferDMA(const uint8_t* buffer, uint16_t size);
+void SPItransferReceiveDMA(const uint8_t* buffer, uint8_t* rx, uint16_t size);
+
+
+
+#endif
---
+++ websocket_client_NUCLEO-F767ZI/rsx_ethernet/demo/st/nucleo_f767zi/web_socket_client_demo/src/PEC.h
@@ -0,0 +1,19 @@
+#ifndef PEC_HEADER
+#define PEC_HEADER
+
+//#ifdef __cplusplus //for c->arduino translation
+//extern "C" {
+//#endif
+
+#define CRC15_POLY 0x4599
+extern uint16_t pec15Table[256];
+
+
+void init_PEC15_Table(); //initializes a pec lookup table for pec0 & pec1 byes
+uint16_t pec15(char *data , int len); //returns 16bit pec for pec[0:1]
+
+//#ifdef __cplusplus
+//}
+//#endif
+
+#endif //PEC_HEADER
