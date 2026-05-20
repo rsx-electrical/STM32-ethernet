6c672f17966d2d0acab10c8a525a5637b8e44303
cocolu76 <116968267+cocolu76@users.noreply.github.com>
2026-05-19 20:29:04 -0400

    merge1

--- rsx_ethernet/demo/st/nucleo_f767zi/web_socket_client_demo/src/BMSlib.c
+++ rsx_ethernet/demo/st/nucleo_f767zi/web_socket_client_demo/src/BMSlib.c
@@ -24,11 +24,8 @@ const uint8_t CLRCELL[4] = {0x7,0x11,0xc9,0xc0};
 volatile uint8_t spiTransferComplete = 0;
 volatile SPI_Command_t spiCmd = SPI_CMD_NONE;
  SPI_HandleTypeDef hspi3;
- //SPI_HandleTypeDef hspi1;
  TaskHandle_t  spiSendTaskHandle;
  TaskHandle_t  bmsTaskHandle;
-// DMA_HandleTypeDef hdma_spi1_tx;
-// DMA_HandleTypeDef hdma_spi1_rx;
  extern uint16_t batt_voltage_mv[NUMCELLS];
 
 void measure_batt_bms(uint16_t* mv, int print){
@@ -40,6 +37,8 @@ void measure_batt_bms(uint16_t* mv, int print){
   rdvab(cell_voltage_100uV, NUMCELLS);
 
   if(print){
+    HAL_Delay(100);
+
     for(int i=0; i < NUMCELLS; i++){
         mv[i] = cell_voltage_100uV[i] / 10; //get in mV
         	TRACE_INFO("C%0d=%d ", i, mv[i]);
@@ -58,48 +57,7 @@ void measure_batt_bms(uint16_t* mv, int print){
     }
 }
 
-/**
-  * @brief SPI3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SPI3_Init(void)
-{
-
-  /* USER CODE BEGIN SPI3_Init 0 */
-
-  /* USER CODE END SPI3_Init 0 */
-
-  /* USER CODE BEGIN SPI3_Init 1 */
-
-  /* USER CODE END SPI3_Init 1 */
-  /* SPI3 parameter configuration*/
-
-  /* USER CODE BEGIN SPI3_Init 2 */
-
-  /* USER CODE END SPI3_Init 2 */
-
-}
-
 void RSX_SPI_Init(void) {
-	/*
-  hspi1.Instance = SPI1;
-  hspi1.Init.Mode = SPI_MODE_MASTER;
-  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
-  hspi1.Init.CLKPolarity = SPI_POLARITY_HIGH;
-  hspi1.Init.CLKPhase = SPI_PHASE_2EDGE;
-  hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
-  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi1.Init.CRCPolynomial = 10;
-
-  HAL_StatusTypeDef status1 = HAL_SPI_Init(&hspi1);
-  if (status1 != HAL_OK) TRACE_INFO("RSX: BMS HAL_SPI_Init failed\n");
-//*/
-
 	  hspi3.Instance = SPI3;
 	  hspi3.Init.Mode = SPI_MODE_MASTER;
 	  hspi3.Init.Direction = SPI_DIRECTION_2LINES;
@@ -112,8 +70,7 @@ void RSX_SPI_Init(void) {
 	  hspi3.Init.TIMode = SPI_TIMODE_DISABLE;
 	  hspi3.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
 	  hspi3.Init.CRCPolynomial = 10;
-	  //hspi3.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-	  //hspi3.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+
 	  HAL_StatusTypeDef status3 = HAL_SPI_Init(&hspi3);
 	  if (status3 != HAL_OK) TRACE_INFO("RSX: BMS HAL_SPI_Init failed\n");
 }
@@ -121,93 +78,6 @@ void RSX_SPI_Init(void) {
 void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle){ //The HAL automatically calls HAL_SPI_MspInit(&hspi3) inside HAL_SPI_Init()
     GPIO_InitTypeDef GPIO_InitStruct = {0}, GPIO_InitStruct_CS = {0};
 
-    if(spiHandle->Instance == SPI1)
-    {
-        /* SPI1 clock enable */
-    	NUCLEO_SPIx_CLK_ENABLE();
-        /* SPI1 GPIO configuration: SCK/ MISO/ MOSI */
-    	NUCLEO_SPIx_SCK_GPIO_CLK_ENABLE();
-    	NUCLEO_SPIx_MISO_MOSI_GPIO_CLK_ENABLE();
-    	/* SPI1 GPIO configuration: CS */
-    	NUCLEO_SPIx_CS_GPIO_CLK_ENABLE();
-
-        GPIO_InitStruct.Pin = NUCLEO_SPIx_SCK_PIN;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        GPIO_InitStruct.Alternate = NUCLEO_SPIx_SCK_AF;  // GPIO_AF5_SPI1, AF5 = SPI1
-        HAL_GPIO_Init(NUCLEO_SPIx_SCK_GPIO_PORT, &GPIO_InitStruct); //GPIO_A for both SCK and MOSI and MISO
-
-        // MOSI (PB5)
-        GPIO_InitStruct.Pin = GPIO_PIN_5;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        GPIO_InitStruct.Alternate = NUCLEO_SPIx_MISO_MOSI_AF;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-        // MISO (PA6)
-        GPIO_InitStruct.Pin = NUCLEO_SPIx_MISO_PIN;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;      // AF_PP works for input as well
-        GPIO_InitStruct.Pull = GPIO_NOPULL;          // or GPIO_PULLUP if desired
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        GPIO_InitStruct.Alternate = NUCLEO_SPIx_MISO_MOSI_AF;
-        HAL_GPIO_Init(NUCLEO_SPIx_MISO_MOSI_GPIO_PORT, &GPIO_InitStruct);
-
-        //config CS (PD14)
-        GPIO_InitStruct_CS.Pin = NUCLEO_SPIx_CS_PIN;
-        GPIO_InitStruct_CS.Mode = GPIO_MODE_OUTPUT_PP;
-        GPIO_InitStruct_CS.Pull = GPIO_NOPULL;
-        GPIO_InitStruct_CS.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        HAL_GPIO_Init(NUCLEO_SPIx_CS_GPIO_PORT, &GPIO_InitStruct_CS); //GPIO_D
-        HAL_GPIO_WritePin(NUCLEO_SPIx_CS_GPIO_PORT, NUCLEO_SPIx_CS_PIN, GPIO_PIN_SET);
-
-        /*
-        __HAL_RCC_DMA2_CLK_ENABLE();
-        // 6) Configure TX DMA (SPI1_TX = DMA2_Stream3_Channel3)
-          hdma_spi1_tx.Instance = DMA2_Stream3;
-          hdma_spi1_tx.Init.Channel = DMA_CHANNEL_3;
-          hdma_spi1_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
-          hdma_spi1_tx.Init.PeriphInc = DMA_PINC_DISABLE;
-          hdma_spi1_tx.Init.MemInc = DMA_MINC_ENABLE;
-          hdma_spi1_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-          hdma_spi1_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-          hdma_spi1_tx.Init.Mode = DMA_NORMAL;
-          hdma_spi1_tx.Init.Priority = DMA_PRIORITY_HIGH;
-          hdma_spi1_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
-
-          HAL_DMA_Init(&hdma_spi1_tx);
-
-          __HAL_LINKDMA(&hspi1, hdmatx, hdma_spi1_tx);
-
-          // 7) Configure RX DMA (SPI1_RX = DMA2_Stream0_Channel3)
-          hdma_spi1_rx.Instance = DMA2_Stream0;
-          hdma_spi1_rx.Init.Channel = DMA_CHANNEL_3;
-          hdma_spi1_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
-          hdma_spi1_rx.Init.PeriphInc = DMA_PINC_DISABLE;
-          hdma_spi1_rx.Init.MemInc = DMA_MINC_ENABLE;
-          hdma_spi1_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-          hdma_spi1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-          hdma_spi1_rx.Init.Mode = DMA_NORMAL;
-          hdma_spi1_rx.Init.Priority = DMA_PRIORITY_HIGH;
-          hdma_spi1_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
-
-          HAL_DMA_Init(&hdma_spi1_rx);
-
-          __HAL_LINKDMA(&hspi1, hdmarx, hdma_spi1_rx);
-
-          // 8) Enable NVIC for DMA
-          HAL_NVIC_SetPriority(DMA2_Stream3_IRQn, 5, 0); // TX
-          HAL_NVIC_EnableIRQ(DMA2_Stream3_IRQn);
-
-          HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 5, 0); // RX
-          HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
-
-          // 9) SPI IRQ (optional if using interrupts)
-          HAL_NVIC_SetPriority(SPI1_IRQn, 5, 0);
-          HAL_NVIC_EnableIRQ(SPI1_IRQn);
-          */
-    }
 	if(spiHandle->Instance==SPI3)
 		{
 		/* Peripheral clock enable */
@@ -270,8 +140,7 @@ void rsxSpiSendTask(void *arg){
 
 void SPItransfer(const uint8_t* buffer, uint16_t size){ //send buffer
     HAL_GPIO_WritePin(SPI3_CS_PORT, SPI3_CS_PIN, GPIO_PIN_RESET);
-	HAL_SPI_Transmit(&hspi3, (uint8_t*)buffer, size, 100);
-	//HAL_SPI_Transmit(&hspi1, (uint8_t*)buffer, size, 100);
+	  HAL_SPI_Transmit(&hspi3, (uint8_t*)buffer, size, 100);
     HAL_GPIO_WritePin(SPI3_CS_PORT, SPI3_CS_PIN, GPIO_PIN_SET);
 }
 
@@ -279,7 +148,6 @@ void SPItransferReceive(const uint8_t* buffer, uint8_t* rx, uint16_t size){
   //send buffer, receive rx at same time. rx and buffer have are "size" bytes
     HAL_GPIO_WritePin(SPI3_CS_PORT, SPI3_CS_PIN, GPIO_PIN_RESET);
 	HAL_SPI_TransmitReceive(&hspi3, (uint8_t*) buffer,rx, size, 100);
-	//HAL_SPI_TransmitReceive(&hspi1, (uint8_t*) buffer,rx, size, 100);
     HAL_GPIO_WritePin(SPI3_CS_PORT, SPI3_CS_PIN, GPIO_PIN_SET);
 }
 
@@ -325,7 +193,11 @@ void rdvab(uint16_t* CV, int CVsize){ //get voltages, CV must has size of 6
   uint8_t datareceived_a[6];
   memcpy(datareceived_a, &rxbuffer_a[4], 6);
   if(!checkPEC(rxbuffer_a[10], rxbuffer_a[11], datareceived_a, 6)){
-    //TRACE_INFO("PEC failed\n");
+	  TRACE_INFO("PEC failed: ");
+	  print_buffer(datareceived_a, sizeof(datareceived_a));
+	  for (int i=0; i < sizeof(datareceived_a); i++){
+		  datareceived_a[i]=0;
+	  }
   }
 
   HAL_Delay(100);
@@ -337,7 +209,11 @@ void rdvab(uint16_t* CV, int CVsize){ //get voltages, CV must has size of 6
   uint8_t datareceived_b[6];
   memcpy(datareceived_b, &rxbuffer_b[4], 6);
   if(!checkPEC(rxbuffer_b[10], rxbuffer_b[11], datareceived_b, 6)){
-    //TRACE_INFO("PEC failed: rxbuffer_b received \n");
+	  TRACE_INFO("PEC failed: ");
+	  print_buffer(datareceived_b, sizeof(datareceived_b));
+	  for (int i=0; i < sizeof(datareceived_b); i++){
+		  datareceived_b[i]=0;
+	  }
   }
   HAL_Delay(100);
 /*
